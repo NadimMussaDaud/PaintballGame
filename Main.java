a5af2633f3164dcbdeddbabf72b53aa9d70d893b
Nadim Daud <95245131+NadimMussaDaud@users.noreply.github.com>
2024-04-26 11:50:39 +0100

    Update Main.java in Fatal Error
    See lines 385-389

--- Main.java
+++ Main.java
@@ -381,16 +381,23 @@ public class Main {
             String team = in.next();
             String bunker = in.nextLine().trim();
 
+
+            if(i == teams-1 && !game.canPlay()){
+                System.out.println(FATAL_ERROR);
+                game = null;
+                break;
+            }
+            
             if(ValidTeam(team, bunker)){
                 game.addTeam(team, bunker);
             }else
                 System.out.println(INVALID_TEAM);
         }
-        
+        /*
         if(!game.canPlay()){
             game = null;
             System.out.println(FATAL_ERROR);
-        }
+        }*/
     }
 
     private static boolean ValidTeam(String team, String bunker) {
